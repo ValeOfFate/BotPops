25f78746b8e0461a051c75c9f690c01d78dc1191
ValeOfFate <61280123+ValeOfFate@users.noreply.github.com>
2023-06-18 02:55:49 -0400

    Config/AutoDC
    Adding config file to store bot token and several channel specific addresses.
    
    Will be useful for storing info outside the bot later

--- PrimeBot.py
+++ PrimeBot.py
@@ -1,14 +1,27 @@
 from discord.ext import commands
 import discord
 from discord import FFmpegPCMAudio
+import json
 
-
+# Config Values
 SklarChannel = ''
+BotToken = ''
+DomainAddress = ''
+BotID = ''
+
+# Bot Creation
 intents = discord.Intents.all()
-
-
 bot = commands.Bot(command_prefix='^', intents=intents)
 
+# Config Loading
+with open('Config.json') as uFile:
+        Data = uFile.read()
+        Obj = json.loads(Data)
+
+BotToken = Obj['BotToken']
+SklarChannel = Obj['SklarChannel']
+DomainAddress = Obj['DomainAddress']
+BotID = Obj['BotID']
 
 # Functions
 @bot.command()
@@ -50,14 +63,25 @@ async def on_message(message):
 @bot.event
 async def on_voice_state_update(member, before, after):
 
-      if before.channel is None and after.channel is not None:
-        if after.channel.id == 774879876670029864:
-            channel = after.channel
-            voice = await channel.connect()
-            source = FFmpegPCMAudio('Files/PM.wav')
-            player = voice.play(source)
-         #do whatever you want here
+    voice_state = member.guild.voice_client
 
+    if member.id != BotID: 
+        if after.channel != None and after.channel.id == DomainAddress and len(after.channel.members) == 1:
+            channel = after.channel
+            BotClient = await channel.connect()
+            source = FFmpegPCMAudio('Files/PM.wav')
+            player = BotClient.play(source)
+
+        if before.channel != None and before.channel.id == DomainAddress and len(before.channel.members) == 1:
+            channel = before.channel
+            await voice_state.disconnect()
+
+    if member.id == BotID and after.channel != None: 
+        if after.channel != None and after.channel.id != DomainAddress:
+            channel = before.channel
+            discon = await voice_state.disconnect(force=True)
+
+    
 
 # Token
-bot.run("")
+bot.run(BotToken)
